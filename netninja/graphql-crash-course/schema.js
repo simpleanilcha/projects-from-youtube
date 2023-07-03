0cd85a209e7c7561eff31811b544287092d4ebfe
Anil Maharjan <simpleanilcha@gmail.com>
2023-07-04 05:13:29 +0545

    Query Variables: get single item using id argument

--- netninja/graphql-crash-course/schema.js
+++ netninja/graphql-crash-course/schema.js
@@ -18,7 +18,10 @@ export const typeDefs = `#graphql
   }
   type Query {
     reviews: [Review]
+    review(id: ID!): Review
     games: [Game]
+    game(id: ID!): Game
     authors: [Author]
+    author(id: ID!): Author
   }
 `
