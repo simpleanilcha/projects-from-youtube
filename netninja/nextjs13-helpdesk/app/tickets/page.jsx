f3f17ba34d634c103239517f613282bca0086f67
Anil Maharjan <simpleanilcha@gmail.com>
2023-08-22 23:37:57 +0545

    06 Fetching & Revalidating data

--- netninja/nextjs13-helpdesk/app/tickets/page.jsx
+++ netninja/nextjs13-helpdesk/app/tickets/page.jsx
@@ -1,7 +1,16 @@
+import TicketList from "./TicketList";
+
 export default function Tickets() {
   return (
     <main>
-      <h2>Tickets</h2>
+      <nav>
+        <div>
+          <h1>Tickets</h1>
+          <p><small>Currently open tickets.</small></p>
+        </div>
+      </nav>
+
+      <TicketList />
     </main>
   )
 }
