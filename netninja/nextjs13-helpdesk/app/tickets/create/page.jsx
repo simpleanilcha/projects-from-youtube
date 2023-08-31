cb29165a07ad3afb12bd587dc6009222dc7941d1
Anil Maharjan <simpleanilcha@gmail.com>
2023-08-31 23:53:23 +0545

    13 Client Form Component

---
+++ netninja/nextjs13-helpdesk/app/tickets/create/page.jsx
@@ -0,0 +1,10 @@
+import CreateForm from './createForm'
+
+export default function CreateTicket() {
+  return (
+    <main>
+      <h2 className="text-primary text-center">Add a New Ticket</h2>
+      <CreateForm />
+    </main>
+  )
+}
